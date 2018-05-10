bcc74153565d01bbb2f11b3e819eff737e61548a
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-10 13:29:56 -0600

    Added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Bottega Mad Libs</h1>
-      </div>
+      <div> 
+        <div className="backgroundSkew">
+         <div className="madlib-heading">
+          <h1>Bottega Mad Libs</h1>
+          <div className="madlib-subheading">
+          Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
+           </div>
+          </div>
+         </div>
+       </div>
     );
   }
 }
