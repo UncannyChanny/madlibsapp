822b6808b2b3c5593872e81b8461b9f189cbe108
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-11 11:45:10 -0600

    Added Clear Button, styled buttons and input

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -56,9 +56,32 @@ class MadlibForm extends Component {
     }
 
     handleSubmit = function(event) {
-        this.setState({completedform: true});
+        this.setState({completedForm: true});
         event.preventDefault();
     }.bind(this);
+
+    handleClick = function() {
+        this.setState ({
+            completedForm: false,
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+        })
+
+    }.bind(this)
+
+    renderButton = function() {
+        if(this.state.completedForm) {
+             return <a className="clear-button" onClick={this.handleClick}>Clear Mad Lib</a>
+        }
+        return <input type="submit" className="generate-button" value="Generate Mad Lib" />
+       
+    }
     
     
     render() {
@@ -71,7 +94,7 @@ class MadlibForm extends Component {
 
             {placeholder: 'Adjective', prop: 'adjectiveTwo', state: this.state.adjectiveTwo},
             {placeholder: 'Noun', prop: 'nounOne', state: this.state.nounOne},
-            {placeholder: 'Number', prop: 'numberOne', state: this.state.NumberOne},
+            {placeholder: 'Number', prop: 'numberOne', state: this.state.numberOne},
             {placeholder: 'Number', prop: 'numberTwo', state: this.state.numberTwo},
         ]
 
@@ -90,7 +113,7 @@ class MadlibForm extends Component {
                    </Row>
                    <Row>
                        <Col md="12" className="button-wrapper">
-                          <input type="submit" className="generate-button" value="Generate Mad Lib" />
+                           {this.renderButton()}
                        </Col>
                      </Row>
 
