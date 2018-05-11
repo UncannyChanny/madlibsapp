8652fa35b97e1dfc01ff8792a877764048cbff82
Chandler Ellis <chandler.ellis47@gmail.com>
2018-05-11 13:23:44 -0600

    Added body content

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -6,6 +6,8 @@ import {
 } from 'reactstrap';
 import _ from 'lodash';
 
+import MadlibContent from './madlib_content';
+
 function MadLibInput(props) {
     return (
         <Col md ="3" className="input-wrapper">
@@ -119,6 +121,7 @@ class MadlibForm extends Component {
 
                 
                 </form>
+                  <MadlibContent data={this.state}/>
               </Card>
             </div>
         
